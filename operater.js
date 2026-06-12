55c2e3f6fa962dd994a12a9a0288617db476e5ac
iswaryab25 <iswarya22619@gmail.com>
2026-06-12 14:02:58 +0530

    operater.js

--- operater.js
+++ operater.js
@@ -34,9 +34,14 @@ console.log(a>1 && b>1)
 console.log(a>1 || b>0)
 console.log(a>0)
 
-//bitwise
+//unary
 console.log("Unary Operator")
 console.log(c++)
 console.log(c--)
 console.log(--c)
 console.log(++c)
+ 
+//ternary
+console.log("Ternary Operator")
+var result =(a>b) ? "a is greater" : "b is greater"
+console.log(result)
