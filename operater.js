0f2390bf4740422c66b267a105cfc2a6528af9db
iswaryab25 <iswarya22619@gmail.com>
2026-06-12 11:35:41 +0530

    operator.js

--- operater.js
+++ operater.js
@@ -7,3 +7,12 @@ console.log(a*b) //multiplication
 console.log(a/b) //division
 console.log(a%b) //modulus
 console.log(a**b)//exponentiation
+ 
+console.log("Assignment operator")
+var c=30
+console.log(a+=30)
+console.log(a-=30)
+console.log(a*=30)
+console.log(a/=30)
+console.log(a%=30)
+console.log(a**=30)
