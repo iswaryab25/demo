1f488b119df3a362177493186ef304554aeee67d
iswaryab25 <iswarya22619@gmail.com>
2026-06-13 13:59:49 +0530

    conditional.js

---
+++ conditional.js
@@ -0,0 +1,16 @@
+console.log("CONDITIONAL STATEMENT");
+
+var light = "green";
+
+if (light == "red") {
+    console.log("Stop");
+}
+else if (light == "white") {
+    console.log("Wait");
+}
+else if (light == "green") {
+    console.log("Go");
+}
+else {
+    console.log("Invalid signal");
+}
